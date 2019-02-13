1958fc501cdc1f922934817c139ac6e580b61530
Manooo0313 <loth.david.mano@hotmail.com>
2019-02-13 14:33:15 +0100

    add playerPlay and computerPlay logic

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-alert("this js file linked properly");
+function computerPlay() {
+    let n = Math.random();
+    if (n <= (1/3)) {
+        return "Rock";
+    } else if (n <= (2/3)) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+function capitalizeFirstLetterAndLowerCaseTheOthers(string) {
+    string = string.toLowerCase();
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function playerPlay() {
+    let selection = prompt ("Enter one of this: Rock, Paper, Scissors!");
+    if (selection != null) {
+        selection = capitalizeFirstLetterAndLowerCaseTheOthers(selection);
+        if (selection == "Rock" || selection == "Paper" || selection == "Scissors") {
+            return selection;
+        } else {
+            alert(`"${selection}" is not a valid choice, please re-enter: Rock, Paper or Scissors`);
+            return playerPlay();
+        }
+    }
+    
+}
