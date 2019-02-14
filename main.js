977ffc82be59498192fcf984c3d44099029d3cfd
Manooo0313 <loth.david.mano@hotmail.com>
2019-02-14 02:37:32 +0100

    add playRound and game functions still need some modifications

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
     let n = Math.random();
     if (n <= (1/3)) {
@@ -25,5 +28,51 @@ function playerPlay() {
             return playerPlay();
         }
     }
-    
 }
+
+// Rock:    - beat Scissors
+//          - tie Rock -> playerSelection === computerSelection
+//          - loose Paper
+//Paper:    - beat Rock
+//          - tie Paper -> playerSelection === computerSelection
+//          - loose Scissors
+//Scissors: - beat Paper
+//          - tie Scissors -> playerSelection === computerSelection
+//          - loose Rock
+
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerPlay();
+    computerSelection = computerPlay();
+    if (playerSelection === computerSelection){
+        return "Tie, " + playerSelection + " is equal with " + computerSelection + "!";
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock" || playerSelection === "Scissors" && computerSelection === "Paper"){
+        playerScore ++;
+        console.log("Player Score is: " + playerScore);
+        return "You win, " + playerSelection + " beat " + computerSelection + "!";
+    } else {
+        computerScore ++;
+        console.log("Computer Score is: " + computerScore);
+        return "Computer win, " + computerSelection + " beat " + playerSelection + "!";
+    }
+}
+
+function game() {
+    while (playerScore != 5 && computerScore != 5) {
+        playRound();
+        if (playerScore === 5){
+            console.log("Congratulation! You beat the Computer! If you want to play again than hit 'Begin' button!");
+            gameOver();
+            break;
+        } else if (computerScore === 5) {
+            console.log("Computer win! If you want to play again than hit 'Begin' button!");
+            gameOver();
+            break;
+        }
+    }
+}
+
+ function gameOver(){
+    playerScore = 0;
+    computerScore = 0;
+} 
